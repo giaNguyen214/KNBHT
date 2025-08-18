99d61cca78b3ec5518fd5cbeced06749387b972e
giaNguyen214 <roy.otaku.214@gmail.com>
2025-08-18 17:21:49 +0700

    fix: comment temporal page

--- src/app/temporal/page.tsx
+++ src/app/temporal/page.tsx
@@ -1,52 +1,53 @@
-// "use client"
+"use client"
 
-// import { 
-//     Box 
-// } from "@mui/material"
+import { 
+    Box 
+} from "@mui/material"
 
-// import DynamicQuery from "@/components/temporalSearch/DynamicQuery"
-// import ImageGallery from "@/components/temporalSearch/ImageGallery"
+import DynamicQuery from "@/components/temporalSearch/DynamicQuery"
+import ImageGallery from "@/components/temporalSearch/ImageGallery"
 
-// import { useSearch } from "@/hooks/search"
-// import { SearchPayload } from "@/types/Search"
+import { useSearch } from "@/hooks/search"
+import { SearchPayload } from "@/types/Search"
 
-// import { useState, useEffect } from "react"
-// import Login from "@/components/utils/Login"
-
-
-// export default function Test() {
-//     // //  logic Login
-//     // const [username, setUsername] = useState("");
-//     // useEffect(() => {
-//     //     const storedUsername = localStorage.getItem("username");
-//     //     if (storedUsername) {
-//     //         setUsername(storedUsername);
-//     //     }
-//     // }, []);
-//     // const [isOpen, setIsOpen] = useState(false)
-//     // const closeModal = () => {
-//     //     setIsOpen(false)
-//     // }
-//     // if (username === undefined || username === "" || !username) {
-//     //     console.log("username", username)
-//     //     return (
-//     //         <Login closeModal={closeModal}/>
-//     //     )
-//     // }
-
-//     const { results, searching, search } = useSearch();
-
-//     const handleSearch = (searchPayload: SearchPayload) => {
-//         search("temporal", searchPayload);
-//     };
+import { useState, useEffect } from "react"
+import Login from "@/components/utils/Login"
 
 
-//     return (
-//         <Box className="h-screen w-screen flex justify-center items-center p-2">
-//             <DynamicQuery handleSearch={handleSearch} searching={searching}/>
+export default function Test() {
+    // //  logic Login
+    // const [username, setUsername] = useState("");
+    // useEffect(() => {
+    //     const storedUsername = localStorage.getItem("username");
+    //     if (storedUsername) {
+    //         setUsername(storedUsername);
+    //     }
+    // }, []);
+    // const [isOpen, setIsOpen] = useState(false)
+    // const closeModal = () => {
+    //     setIsOpen(false)
+    // }
+    // if (username === undefined || username === "" || !username) {
+    //     console.log("username", username)
+    //     return (
+    //         <Login closeModal={closeModal}/>
+    //     )
+    // }
+
+    const { results, searching, search } = useSearch();
+
+    const handleSearch = (searchPayload: SearchPayload) => {
+        search("temporal", searchPayload);
+    };
+
+
+    return (
+        // <Box className="h-screen w-screen flex justify-center items-center p-2">
+        //     <DynamicQuery handleSearch={handleSearch} searching={searching}/>
         
 
-//             <ImageGallery results={results} cols={5} /> 
-//         </Box>
-//     )
-// }
+        //     <ImageGallery results={results} cols={5} /> 
+        // </Box>
+        <Box></Box>
+    )
+}
